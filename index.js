a65ecd1a96790db12064c1042635b9293226c2b3
FPierre <pflauder@gmail.com>
2017-11-29 16:47:43 +0100

    Use script in CI (backup path in parameter)

--- index.js
+++ index.js
@@ -4,18 +4,14 @@ if (process.platform !== 'linux') {
   throw new Error('linux-user must be running on Linux')
 }
 
-// if (process.argv.length <= 2) {
-//   console.log("Usage: " + __filename + " path/to")
-//   process.exit(-1)
-// }
+if (process.argv.length <= 2) {
+  console.log(`Usage: ${__filename} path/to`)
+  process.exit(-1)
+}
 
-// const path = process.argv[2]
+const path = process.argv[2]
 
-const BACKUP_PATH = './backup.tar.gz'
-
-fileStats(BACKUP_PATH).then(d => {
-  console.log(d)
-})
+fileStats(path).then(d => console.log(d))
 
 // fs.readJson('./package.json')
 //   .then(packageObj => {
