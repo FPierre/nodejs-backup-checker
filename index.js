84e9270854316b583091f11b1b7193bc8035de88
FPierre <pflauder@gmail.com>
2017-11-29 16:45:21 +0100

    Improve file functions

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { users, groups } = require('./src/utils')
+const { fileStats } = require('./src/utils')
 
 if (process.platform !== 'linux') {
   throw new Error('linux-user must be running on Linux')
@@ -8,57 +8,19 @@ if (process.platform !== 'linux') {
 //   console.log("Usage: " + __filename + " path/to")
 //   process.exit(-1)
 // }
-//
+
 // const path = process.argv[2]
 
 const BACKUP_PATH = './backup.tar.gz'
 
-users().then(u => console.log(u))
-groups().then(u => console.log(u))
+fileStats(BACKUP_PATH).then(d => {
+  console.log(d)
+})
 
-// OPTIMIZE
-const fileModeToHexa = mode => {
-  let access = ''
-
-  access = access.concat(mode & 1 ? 'x' : '-',)
-  access = access.concat(mode & 2 ? 'w' : '-',)
-  access = access.concat(mode & 4 ? 'r' : '-',)
-  access = access.concat(mode & 10 ? 'x' : '-',)
-  access = access.concat(mode & 20 ? 'w' : '-',)
-  access = access.concat(mode & 40 ? 'r' : '-',)
-  access = access.concat(mode & 100 ? 'x' : '-',)
-  access = access.concat(mode & 200 ? 'w' : '-',)
-  access = access.concat(mode & 400 ? 'r' : '-')
-
-  return access
-}
-
-const fileStats = async path => {
-  const { birthtime, gid, mode, size, uid } = await fs.stat(path)
-
-  return {
-    access: fileModeToHexa(mode),
-    birthtime,
-    gid,
-    path,
-    size: size / 1000000.0,
-    uid
-  }
-}
-
-
-
-// fileStats(BACKUP_PATH).then(d => {
-//   console.log(d)
-// })
-
-
-/*
-fs.readJson('./package.json')
-  .then(packageObj => {
-    console.log(packageObj.version)
-  })
-  .catch(err => {
-    console.error(err)
-  })
-*/
+// fs.readJson('./package.json')
+//   .then(packageObj => {
+//     console.log(packageObj.version)
+//   })
+//   .catch(err => {
+//     console.error(err)
+//   })
--- src/utils.js
+++ src/utils.js
@@ -1,48 +1,75 @@
 const fs = require('fs-extra')
 
-const users = async () => {
-  let content = null
+// OPTIMIZE
+const fileModeToHexa = mode => {
+  let access = ''
 
-  try {
-    content = await fs.readFile('/etc/passwd')
-  } catch (e) {
-    console.log(e)
-  }
+  access = access.concat(mode & 1 ? 'x' : '-',)
+  access = access.concat(mode & 2 ? 'w' : '-',)
+  access = access.concat(mode & 4 ? 'r' : '-',)
+  access = access.concat(mode & 10 ? 'x' : '-',)
+  access = access.concat(mode & 20 ? 'w' : '-',)
+  access = access.concat(mode & 40 ? 'r' : '-',)
+  access = access.concat(mode & 100 ? 'x' : '-',)
+  access = access.concat(mode & 200 ? 'w' : '-',)
+  access = access.concat(mode & 400 ? 'r' : '-')
 
-  let users = content.toString().split('\n')
-
-  return users.map(user => {
-    const [username, , uid] = user.split(':')
-
-    return {
-      username,
-      uid: Number(uid)
-    }
-  })
+  return access
 }
 
-const groups = async () => {
-  let content = null
-
+const systemUsers = async () => {
   try {
-    content = await fs.readFile('/etc/group')
+    let content = await fs.readFile('/etc/passwd')
+    const users = content.toString().split('\n')
+
+    return users.map(user => user.split(':')[0])
   } catch (e) {
     console.log(e)
   }
+}
 
-  const groups = content.toString().split('\n')
+const systemGroups = async () => {
+  try {
+    let content = await fs.readFile('/etc/group')
+    const groups = content.toString().split('\n')
 
-  return groups.map(group => {
-    const [groupname, , gid] = group.split(':')
+    return groups.map(group => group.split(':')[0])
 
-    return {
-      groupname,
-      gid: Number(gid)
-    }
-  })
+      // const [name, , gid] = group.split(':')
+      //
+      // return {
+      //   name,
+      //   gid: Number(gid)
+      // }
+    // })
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+const fileStats = async path => {
+  const { birthtime, gid, mode, size, uid } = await fs.stat(path)
+
+  const access = fileModeToHexa(mode)
+
+  // OPTIMIZE: do not change often, use cache?
+  // https://github.com/wxygeek/linux-user/blob/master/lib/user.js
+  const users = await systemUsers()
+  const groups = await systemGroups()
+
+  const user = users.find(user => user.uid = uid)
+  const group = groups.find(group => group.uid = uid)
+
+  return {
+    access,
+    birthtime,
+    group,
+    path,
+    size: size / 1000000.0,
+    user
+  }
 }
 
 module.exports = {
-  users,
-  groups
+  fileStats
 }
